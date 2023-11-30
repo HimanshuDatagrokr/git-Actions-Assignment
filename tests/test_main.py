a51d1d94bf3e52a1be7371bbcad62ed3b996fc59
himanshu <himanshu.prasad@datagrokr.com>
2023-11-30 10:30:52 +0530

    first commit

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+from main import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_add_negative_numbers(self):
+        result = add(-2, 3)
